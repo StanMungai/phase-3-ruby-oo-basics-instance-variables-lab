d48e6f164a3829d2a2764af9927bc87b487d8b20
stanford kangethe <stanfordkangethe16@gmail.com>
2022-08-22 21:44:30 +0300

    setters and getters

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
 class Dog
 
+  # setter method
+  def name=(value)
+    # assign the @name instance variable a value of whatever is passed as an argument
+    @this_dogs_name = value
+  end
+
+  # getter method
+  def name
+    # access the data from the @name instance variable and return it
+    @this_dogs_name
+  end
+
 end
+
+odie = Dog.new
+odie.name = "Odie"
+
+puts odie.name
